a5e2b7f9226f61c07fd4f2fe9d8249cc36b2df10
Inayah25 <100848480+Inayah25@users.noreply.github.com>
2025-08-24 14:56:54 +1200

    Services
    HomepageServices added in name folder for now. router used in app.js to navigate between profilescreen and services

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 import ProfileScreen from "./Inayah/ProfileScreen";
+import HomePageServices from "./Inayah/HomePageServices";
 
 function App() {
   return (
-     <div>
-      <ProfileScreen />
-    </div>
+     <BrowserRouter>
+      <Routes>
+        {/* default -> services */}
+        <Route path="/" element={<Navigate to="/services" replace />} />
+        <Route path="/services" element={<HomePageServices />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+        {/* 404 */}
+        <Route path="*" element={<div style={{ padding: 24 }}>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
