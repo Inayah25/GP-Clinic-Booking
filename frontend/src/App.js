6d9e3c24540a1204b38206155021093c1bd92aa0
YuNandaZaw <yulay30@gmail.com>
2025-09-10 23:56:57 +1200

    Added Health Dashboard
    Added Health Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ProfileScreen from "./Inayah/ProfileScreen";
 import HomePageServices from "./Inayah/HomePageServices";
 import HomePage from "./yu/homepage";
